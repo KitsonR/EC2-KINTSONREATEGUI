55c22721c5be09234237088a02c89d48210f9329
KitsonR <reateguikitson98@gmail.com>
2022-06-29 01:30:39 -0500

    EC2

---
+++ EC2-KINTSONREATEGUI/src/main/java/com/idat/EC2KINTSONREATEGUI/Ec2KintsonreateguiApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EC2KINTSONREATEGUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2KintsonreateguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2KintsonreateguiApplication.class, args);
+	}
+
+}
---
+++ EC2-KINTSONREATEGUI/src/test/java/com/idat/EC2KINTSONREATEGUI/Ec2KintsonreateguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EC2KINTSONREATEGUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2KintsonreateguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
